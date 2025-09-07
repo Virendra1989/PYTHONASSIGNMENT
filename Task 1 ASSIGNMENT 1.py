ac268f99a593dfde61dda28a6fe66f27479a68b0
Virendra1989 <virendrayadav7978@gmail.com>
2025-09-07 19:02:39 +0530

    Add files via upload

---
+++ Task 1 ASSIGNMENT 1.py
@@ -0,0 +1,10 @@
+first = int(input("Enter the first number: ")) 
+second = int(input("Enter the second number: "))  
+
+print("Addition:", first + second)
+print("Subtraction:", first - second)
+print("Multiplication:", first * second)
+print("Division:", first / second)
+
+
+
---
+++ Task 1 ASSIGNMENT 2.py
@@ -0,0 +1,11 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is an even number")
+    else:
+        print(f"{number} is an odd number")
+
+# Example usage
+num = int(input("Enter a number: "))
+check_even_odd(num)
+
+
