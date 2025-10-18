52e6c5e15374efc30fabb76541e3be2aafb99a3d
Virendra1989 <virendrayadav7978@gmail.com>
2025-10-18 21:37:26 +0530

    Add files via upload

---
+++ ASSIGNMENT 5 TASK 2.py
@@ -0,0 +1,9 @@
+numbers=list(range(1,11))
+
+first_five=numbers[:5]
+
+rev=sorted((first_five),reverse=True)
+
+print("Original List :",numbers)
+print("Extracted First Five Element :",first_five)
+print("Reversed Extracted Element :",rev)
